7bcec77bb2f52245d0772d703d8d083c6d170a3c
Admin9705 <user@dunn.cloud>
2019-01-21 12:41:48 -0500

    update

--- emails.sh
+++ emails.sh
@@ -47,4 +47,3 @@ echo "NOTE 1: Make sure you SHARE with the CORRECT TEAM DRIVE!"
 echo "NOTE 2: Save Time & Copy & Paste the E-Mails Into the G-Drive Share!"
 echo
 cat /tmp/pgblitz.emails.list
-echo "INFO - PGBlitz: Displayed to User the E-Mails" > /var/plexguide/pg.log && bash /opt/pgclone/log/log.sh
