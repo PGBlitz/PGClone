4c23e875f059efc4efb86e8acbae644c3a2a58da
Robert Baker <se7enalive@gmail.com>
2019-01-26 19:29:14 -0700

    fix move

--- templates/move_script.sh
+++ templates/move_script.sh
@@ -8,14 +8,16 @@
 if pidof -o %PPID -x "$0"; then
    exit 1
 fi
-
+# Outside Variables
+dlpath=$(cat /var/plexguide/server.hd.path)
 sleep 10
 while true
 do
+dlpath=$(cat /var/plexguide/server.hd.path)
 ## Sync, Sleep 2 Minutes, Repeat. BWLIMIT 9 Prevents Google 750GB Google Upload Ban
 rclone move --config /opt/appdata/plexguide/rclone.conf --bwlimit {{bandwidth.stdout}}M \
   --tpslimit 6 --exclude='**partial~' --exclude="**_HIDDEN~" --exclude=".unionfs/**" \
   --exclude=".unionfs-fuse/**" --checkers=16 --max-size 99G --log-file=/opt/appdata/plexguide/rclone \
-  --log-level INFO --stats 5s {{hdpath.stdout}}/move {{ver.stdout}}:/
+  --log-level INFO --stats 5s $dlpath/move {{ver.stdout}}:/
 sleep 10
 done
