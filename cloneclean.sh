4a850be6e855f938719c6cb67d9e45e811f5a7c5
Robert Baker <se7enalive@gmail.com>
2019-01-28 11:54:02 -0700

    fix removing empty directories and setting permisisons

--- cloneclean.sh
+++ cloneclean.sh
@@ -9,12 +9,13 @@ mkdir -p "$dlpath/pgblitz/upload"
 mkdir -p "$dlpath/move"
 
 # Permissions
-chown 1000:1000 "$dlpath/move"
-chown 1000:1000 "$dlpath/pgblitz/upload"
-chmod 755 "$dlpath/move"
-chown 755 "$dlpath/pgblitz/upload"
+chown -R 1000:1000 "$dlpath/move"
+chown -R 1000:1000 "$dlpath/pgblitz/upload"
+chmod -R 755 "$dlpath/move"
+chown -R 755 "$dlpath/pgblitz/upload"
 
 # Execution
-find "$dlpath/downloads" -mindepth 2 ! -path **nzbget/* ! -path **sabnzbd/* ! -path **qbittorrent/* ! -path **deluge/* ! -path **rutorrent/* ! -path **deluge/* ! -path **transmission/* -mmin +5 -type d -empty -delete
-find "$dlpath/move/" -mindepth 2 -mmin +30 -type d -empty -delete
+find "$dlpath/downloads" -mindepth 2 ! -path **nzbget*/* ! -path **sabnzbd*/* ! -path **qbittorrent*/* ! -path **deluge*/* ! -path **rutorrent*/* ! -path **deluge*/* ! -path **transmission*/* -mmin +5 -type d -empty -delete
+find "$dlpath/downloads" -mindepth 3 -mmin +5 -type d -empty -delete
+find "$dlpath/move" -mindepth 2 -mmin +30 -type d -empty -delete
 find "$dlpath/pgblitz/upload" -mindepth 1 -mmin +30 -type d -empty -delete
--- templates/pgblitz.sh
+++ templates/pgblitz.sh
@@ -37,6 +37,12 @@ while [ 1 ]; do
   dlpath=$(cat /var/plexguide/server.hd.path)
   mkdir -p /$dlpath/pgblitz/upload
 
+  # Permissions
+  chown -R 1000:1000 "$dlpath/move"
+  chown -R 1000:1000 "$dlpath/pgblitz/upload"
+  chmod -R 755 "$dlpath/move"
+  chown -R 755 "$dlpath/pgblitz/upload"
+
   if [ "$keylast" == "$keyuse" ]; then keycurrent=0; fi
 
   let "keycurrent++"
@@ -89,8 +95,9 @@ while [ 1 ]; do
   sleep 30
 
 # Remove empty directories
-find "$dlpath/downloads" -mindepth 2 ! -path **nzbget/* ! -path **sabnzbd/* ! -path **qbittorrent/* ! -path **deluge/* ! -path **rutorrent/* ! -path **deluge/* ! -path **transmission/* -mmin +5 -type d -empty -delete
-find "$dlpath/move/" -mindepth 2 -mmin +5 -type d -empty -delete
-find "$dlpath/pgblitz/upload" -mindepth 1 -type d -empty -delete
+find "$dlpath/downloads" -mindepth 2 ! -path **nzbget*/* ! -path **sabnzbd*/* ! -path **qbittorrent*/* ! -path **deluge*/* ! -path **rutorrent*/* ! -path **deluge*/* ! -path **transmission*/* -mmin +5 -type d -empty -delete
+find "$dlpath/downloads" -mindepth 3 -mmin +5 -type d -empty -delete
+find "$dlpath/move" -mindepth 2 -mmin +30 -type d -empty -delete
+find "$dlpath/pgblitz/upload" -mindepth 1 -mmin +30 -type d -empty -delete
 
 done
