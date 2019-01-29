5ac0054f025c1aedcec2f46bc04ae4d7566c90c0
Robert Baker <se7enalive@gmail.com>
2019-01-29 15:30:46 -0700

    sync_read, caching, symlinks

--- templates/pgunion.sh
+++ templates/pgunion.sh
@@ -10,4 +10,4 @@
 dlpath=$(cat /var/plexguide/server.hd.path)
 sleep 10
 
-mergerfs -o defaults,noauto_cache,allow_other,use_ino,func.getattr=newest,nonempty,direct_io,security_capability=false,xattr=nosys,category.create=ff,minfreespace=0,umask=002,noatime,fsname=pgUnion /mnt/gdrive=NC:/mnt/tdrive=NC:/mnt/tcrypt=NC:/mnt/gcrypt=NC:$dlpath/downloads=NC:$dlpath/move=RW /mnt/unionfs
+mergerfs -o symlinkify=true,symlinkify_timeout=90,atomic_o_trunc,big_writes,default_permissions,splice_move,splice_read,splice_write,auto_cache,sync_read,allow_other,use_ino,func.getattr=newest,nonempty,direct_io,security_capability=false,xattr=nosys,category.create=ff,minfreespace=0,umask=002,noatime,fsname=pgUnion /mnt/gdrive=NC:/mnt/tdrive=NC:/mnt/tcrypt=NC:/mnt/gcrypt=NC:$dlpath/downloads=NC:$dlpath/move=RW /mnt/unionfs
