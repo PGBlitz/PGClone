23af2ef9e22c84440aa2e202996dc252a9e18288
Admin9705 <user@dunn.cloud>
2019-02-03 06:10:57 -0500

    update

--- gdrive.sh
+++ gdrive.sh
@@ -12,7 +12,7 @@ source /opt/pgclone/functions/pgclone.sh
 ################################################################################
 
 ################################ Forces RClone Installer ######## START
-echo "14" > /var/plexguide/pg.rcloneprime
+echo "15" > /var/plexguide/pg.rcloneprime
 
 core () {
     touch /var/plexguide/pg."${1}".stored
