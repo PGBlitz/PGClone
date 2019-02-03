cd73f2bcddc7934a99407ae8a043d4a2039d8971
Robert Baker <se7enalive@gmail.com>
2019-02-02 18:14:35 -0700

    fix

--- functions/pgblitz.sh
+++ functions/pgblitz.sh
@@ -7,7 +7,7 @@
 ################################################################################
 function log()
 {
-    echo "[PGBlitz] $@" > /var/plexguide/pg.log && bash /opt/plexguide/menu/log/log.sh
+    echo "[PGBlitz] $@" > /var/plexguide/logs/pg.log && bash /opt/plexguide/menu/log/log.sh
     echo "[PGBlitz] $@"
 }
 
