d8ea91c247e45c3fe351fd735a9a700517d8b308
Admin9705 <user@dunn.cloud>
2019-03-04 10:29:38 -0500

    update

--- functions/functions.sh
+++ functions/functions.sh
@@ -78,6 +78,56 @@ tee <<-EOF
 ━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━
 EOF
 read -p '↘️  Input Selection | Press [ENTER]: ' typed < /dev/tty
+clonestartactions
+}
+
+clonestartactions () {
+if [[ "$transport" == "mu" ]]; then
+  case $typed in
+      1 )
+      echo "mu" > /var/plexguide/pgclone.transport ;;
+      2 )
+      echo "me" > /var/plexguide/pgclone.transport ;;
+      3 )
+      echo "bu" > /var/plexguide/pgclone.transport ;;
+      4 )
+      echo "be" > /var/plexguide/pgclone.transport ;;
+      5 )
+      echo "pd" > /var/plexguide/pgclone.transport ;;
+      6 )
+      echo "le" > /var/plexguide/pgclone.transport ;;
+      z )
+          exit ;;
+      Z )
+          exit ;;
+      * )
+          clonestart ;;
+elif [[ "$transport" == "me" ]]; then
+tee <<-EOF
+[1] Client ID & Secret: [Fill Me]
+[2] GDrive OAuth:       [Fill Me]
+[3] TDrive OAuth:       [Fill Me]
+[4] TDrive Label:       [None]
+[5] Passwords:          [Not Set]
+EOF
+elif [[ "$transport" == "bu" ]]; then
+tee <<-EOF
+[1] Client ID & Secret: [Fill Me]
+[2] GDrive OAuth:       [Fill Me]
+[3] TDrive OAuth:       [Fill Me]
+[4] TDrive Label:       [None]
+[5] Key Management:     [0] Built
+EOF
+elif [[ "$transport" == "be" ]]; then
+tee <<-EOF
+[1] Client ID & Secret: [Fill Me]
+[2] GDrive OAuth:       [Fill Me]
+[3] TDrive OAuth:       [Fill Me]
+[4] TDrive Label:       [None]
+[5] Passwords:          [Not Set]
+[6] Key Management:     [0] Built
+EOF
+fi
 }
 
 removepgservices () {
