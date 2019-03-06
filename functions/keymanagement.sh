f2df8bfc9a1f1f19ae715081594291523f769865
Admin9705 <user@dunn.cloud>
2019-03-06 00:57:56 -0500

    update

--- functions/keymanagement.sh
+++ functions/keymanagement.sh
@@ -35,7 +35,7 @@ gcloud info | grep Account: | cut -c 10- > /var/plexguide/project.account
 account=$(cat /var/plexguide/project.account)
 
 testcheck=$(gcloud auth list | grep "$typed")
-if [[ "testcheck" == "" ]]; then
+if [[ "$testcheck" == "" ]]; then
 echo
 echo "Check: E-Mail Address Added Failed"
 read -p '↘️  Acknowledge Info | Press [ENTER] ' typed < /dev/tty
