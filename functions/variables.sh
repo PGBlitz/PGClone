bf95af2dbc2da36dd630df8237d0fa3e573dc73e
Admin9705 <user@dunn.cloud>
2019-03-08 10:31:25 -0500

    update

--- functions/variables.sh
+++ functions/variables.sh
@@ -8,7 +8,6 @@
 pgclonevars () {
   mkdir -p /var/plexguide/rclone
   variable /var/plexguide/project.account "NOT-SET"
-  variable /var/plexguide/pgclone.teamdrive ""
   variable /var/plexguide/rclone/deploy.version "null"
   variable /var/plexguide/pgclone.transport "NOT-SET"
   variable /var/plexguide/gdrive.pgclone "⚠️  Not Activated"
@@ -19,6 +18,9 @@ pgclonevars () {
   variable /var/plexguide/pgclone.salt ""
 
   transport=$(cat /var/plexguide/pgclone.transport)
+
+  variable /var/plexguide/pgclone.teamdrive  "NOT-SET"
+  tdname=$(cat /var/plexguide/pgclone.public)
 
   variable /var/plexguide/pgclone.id "NOT-SET" # output for front interface, changes when users sets id/secret
   pgcloneid=$(cat /var/plexguide/pgclone.id)
