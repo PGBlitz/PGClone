cef930ef6f49104b7f62a37119ae7ff3927f0555
Admin9705 <user@dunn.cloud>
2019-03-08 00:17:30 -0500

    update

--- functions/oauth.sh
+++ functions/oauth.sh
@@ -5,12 +5,12 @@
 # URL:        https://pgblitz.com - http://github.pgblitz.com
 # GNU:        General Public License v3.0
 ################################################################################
-source /opt/pgclone/functions/oauth.sh
+source /opt/pgclone/functions/functions.sh
+source /opt/pgclone/functions/variables.sh
 # BAD INPUT
 #oauth () {
 
 pgclonevars
-name="TEST"
 tee <<-EOF
 
 ━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━
@@ -25,8 +25,9 @@ EOF
   read -p '↘️  Token | PRESS [ENTER]: ' token < /dev/tty
 
   if [[ "$token" = "exit" ]]; then mountsmenu; fi
-  curl --request POST --data "code=$token&client_id=$pgclonepublic&client_secret=$pgclonesecret&redirect_uri=urn:ietf:wg:oauth:2.0:oob&grant_type=authorization_code" https://accounts.google.com/o/oauth2/token > /var/plexguide/pgtokentm.output
+  curl --request POST --data "code=${token}&client_id=${pgclonepublic}&client_secret=${pgclonesecret}&redirect_uri=urn:ietf:wg:oauth:2.0:oob&grant_type=authorization_code" https://accounts.google.com/o/oauth2/token > /var/plexguide/pgtokentm.output
   cat /var/plexguide/pgtokentm.output | grep access_token | awk '{ print $2 }' | cut -c2- | rev | cut -c3- | rev > /var/plexguide/pgtokentm2.output
-  curl -H "GData-Version: 3.0" -H "Authorization: Bearer $primet" https://www.googleapis.com/drive/v3/teamdrives > /var/plexguide/teamdrive.output
+  primet=$(cat /var/plexguide/pgtokentm.output)
+  curl -H "GData-Version: 3.0" -H "Authorization: Bearer $primet" https://www.googleapis.com/drive/v3/teamdrives
 
 #}
