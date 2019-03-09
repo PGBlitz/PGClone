f3fe2991aeceb6003a1adb5a63039a67a35811ad
Admin9705 <user@dunn.cloud>
2019-03-09 00:48:17 -0500

    update

--- functions/clonestartoutput.sh
+++ functions/clonestartoutput.sh
@@ -16,7 +16,7 @@ elif [[ "$transport" == "me" ]]; then
 tee <<-EOF
 [1] Client ID & Secret  [${pgcloneid}]
 [2] Passwords           [Not Set]
-[3] GDrive OAuth        [$gstatus]
+[3] GDrive OAuth        [$gstatus] - GCrypt [$gcstatus]
 EOF
 elif [[ "$transport" == "bu" ]]; then
 tee <<-EOF
@@ -31,8 +31,8 @@ tee <<-EOF
 [1] Client ID & Secret  [${pgcloneid}]
 [2] TDrive Label        [$tdname]
 [3] Passwords           [Not Set]
-[4] GDrive OAuth        [$gstatus] - GCrypt [$gstatus]
-[5] TDrive OAuth        [$tstatus] - TCrypt [$tstatus]
+[4] GDrive OAuth        [$gstatus] - GCrypt [$gcstatus]
+[5] TDrive OAuth        [$tstatus] - TCrypt [$tcstatus]
 [6] Key Management      [0] Built
 EOF
 fi
