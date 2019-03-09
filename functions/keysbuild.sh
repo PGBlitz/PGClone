9997506bf5dfbc04d3d7f51d23258baadf9e803a
Admin9705 <user@dunn.cloud>
2019-03-09 12:39:46 -0500

    update

---
+++ functions/keysbuild.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+#
+# Title:      PGBlitz (Reference Title File)
+# Author(s):  Admin9705
+# URL:        https://pgblitz.com - http://github.pgblitz.com
+# GNU:        General Public License v3.0
+################################################################################
+keystart () {
+
+tee <<-EOF
+
+━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━
+🚀 Key Builder ~ http://pgclone.pgblitz.com
+━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━
+QUESTION - Create how many keys for PGBlitz? (From 2 thru 20 )
+
+MATH:
+2  Keys = 1.5 TB Daily | 6  Keys = 4.5 TB Daily
+10 Keys = 7.5 TB Daily | 20 Keys = 15  TB Daily
+
+NOTE 1: Creating more keys DOES NOT SPEED up your transfers
+NOTE 2: Realistic key generation for most are 6 keys
+NOTE 3: Generating 100 keys over time, you must delete them all to create
+        more, which is why making tons of keys is not ideal!
+
+━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━
+EOF
+  read -p '↘️  Type a Number [ 2 thru 20 ] | Press [ENTER]: ' typed < /dev/tty
+
+if [[ "$typed" -le "0" || "$typed" -ge "21" ]]; then keystart
+elif [[ "$typed" -ge "1" && "$typed" -le "20" ]]; then keyphase2
+else keystart; fi
+}
+
+keyphase2 () {
+
+
+
+}
