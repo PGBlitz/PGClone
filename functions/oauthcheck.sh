c18abc78331e901fa8dbb14081ba7bc7bc0c5e3c
Admin9705 <user@dunn.cloud>
2019-03-09 05:08:06 -0500

    update

--- functions/oauthcheck.sh
+++ functions/oauthcheck.sh
@@ -5,8 +5,37 @@
 # URL:        https://pgblitz.com - http://github.pgblitz.com
 # GNU:        General Public License v3.0
 ################################################################################
-oauthgdrive () {
+oauthcheck () {
+pgclonevars
+oauthcheck=gdrive
+tee <<-EOF
 
+━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━
+🚀 Conducting Validation Checks: $oauthcheck
+━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━
 
+EOF
+  rclone mkdir --config /opt/appdata/plexguide/.$oauthcheck $oauthcheck:/plexguide
+  rcheck=$(rclone lsd --config /opt/appdata/plexguide/.$oauthcheck $oauthcheck: | grep -oP plexguide | head -n1)
 
+  if [ "$rcheck" != "plexguide" ];then
+tee <<-EOF
+
+━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━
+⛔  Validation Checks Failed: $oauthcheck
+━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━
+
+NOTES:
+1. Did you set up your $oauthcheck accordingly to the wiki?
+
+EOF
+    read -p '↘️  Acknowledge Info | Press [ENTER] ' typed2 < /dev/tty
+    clonestart
+else
+tee <<-EOF
+━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━
+🚀 System Message: Validation Checks Passed - $oauthcheck
+━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━
+EOF
+  fi
 }
