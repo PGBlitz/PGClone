16a3e555ad772d35d1049a5cfa6cd7990e990180
Admin9705 <user@dunn.cloud>
2019-03-10 00:39:57 -0500

    update

--- functions/deploy.sh
+++ functions/deploy.sh
@@ -6,6 +6,7 @@
 # GNU:        General Public License v3.0
 ################################################################################
 deploypgblitz () {
+# RCLONE BUILD
 echo "#------------------------------------------" >> /opt/appdata/plexguide/rclone.conf
 echo "#RClone Rewrite | Visit https://pgblitz.com" > /opt/appdata/plexguide/rclone.conf
 echo "#------------------------------------------" >> /opt/appdata/plexguide/rclone.conf
@@ -21,4 +22,57 @@ if [ -e "/opt/appdata/plexguide/.tcrypt" ]; then
 cat /opt/appdata/plexguide/.tcrypt >> /opt/appdata/plexguide/tclone.conf; fi
 
 cat /opt/appdata/plexguide/.keys >> /opt/appdata/plexguide/rclone.conf
+
+deployblitzcheck
+}
+
+deployblitzcheck () {
+
+tee <<-EOF
+
+━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━
+🚀 Conducting RClone Mount Checks ~ pgclone.pgblitz.com
+━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━
+
+EOF
+
+ginital=$(rclone lsd --config /opt/appdata/plexguide/rclone.conf gdrive: | grep -oP plexguide | head -n1)
+tinital=$(rclone lsd --config /opt/appdata/plexguide/rclone.conf tdrive: | grep -oP plexguide | head -n1)
+kinital=$(rclone lsd --config /opt/appdata/plexguide/rclone.conf GDSA01: | grep -oP plexguide | head -n1)
+
+if [[ "$ginital" != "plexguide" ]]; then
+  rclone mkdir --config /opt/appdata/plexguide/rclone.conf gdrive:/plexguide; fi
+if [[ "$tinital" != "plexguide" ]]; then
+  rclone mkdir --config /opt/appdata/plexguide/rclone.conf tdrive:/plexguide; fi
+if [[ "$kinital" != "plexguide" ]]; then
+  rclone mkdir --config /opt/appdata/plexguide/rclone.conf GDSA01:/plexguide; fi
+
+gsecond=$(rclone lsd --config /opt/appdata/plexguide/rclone.conf gdrive: | grep -oP plexguide | head -n1)
+tsecond=$(rclone lsd --config /opt/appdata/plexguide/rclone.conf tdrive: | grep -oP plexguide | head -n1)
+ksecond=$(rclone lsd --config /opt/appdata/plexguide/rclone.conf GDSA01: | grep -oP plexguide | head -n1)
+
+if [[ "$gsecond" == "plexguide" ]]; then echo "GDRIVE: Passed"; else echo "GDRIVE: Failed"; fi
+if [[ "$tsecond" == "plexguide" ]]; then echo "TDRIVE: Passed"; else echo "GDRIVE: Failed"; fi
+if [[ "$ksecond" == "plexguide" ]]; then echo "GDSA01: Passed"; else echo "GDSA01: Failed"; fi
+
+if [[ "$gsecond" == "plexguide" || "$tsecond" == "plexguide" || "$ksecond" == "plexguide" ]]; then
+  read -p '↘️  BLITZ DEPLOY NOT READY | Press [ENTER] ' typed2 < /dev/tty
+  #pgblitzpass
+fi
+else
+tee <<-EOF
+
+━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━
+🚀 RClone Mount Checks - Failed
+━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━
+
+POSSIBLE REASONS:
+1. GSuite Account is no longer valid or suspended
+2. User forgot to share out GDSA E-Mail to Team Drive
+3. Conducted a Blitz Key restore and keys are no longer valid
+
+━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━
+EOF
+read -p '↘️  Acknowledge Info | Press [ENTER] ' typed2 < /dev/tty
+clonestart
 }
