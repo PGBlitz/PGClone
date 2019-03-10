c9cb5bcfc5eb32b10f9dc396f619de24116cf255
Admin9705 <user@dunn.cloud>
2019-03-09 21:42:58 -0500

    update

--- functions/deploy.sh
+++ functions/deploy.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #
 # Title:      PGBlitz (Reference Title File)
-# Author(s):  Admin9705 ~ Physik - FlickerRate
+# Author(s):  Admin9705
 # URL:        https://pgblitz.com - http://github.pgblitz.com
 # GNU:        General Public License v3.0
 ################################################################################
