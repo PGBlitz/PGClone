2f6fb6df14dda570079dc4aa03568eb98321c188
Robert Baker <se7enalive@gmail.com>
2019-03-11 13:57:21 -0700

    Update pgunion.sh

--- templates/pgunion.sh
+++ templates/pgunion.sh
@@ -11,4 +11,4 @@ dlpath=$(cat /var/plexguide/server.hd.path)
 sleep 2
 
 mergerfs -o sync_read,allow_other,direct_io,use_ino,func.getattr=newest,nonempty,security_capability=false,xattr=nosys,category.create=ff,umask=002,fsname=pgunion \
-$dlpath/move=RW:$dlpath/downloads=RW:$dlpath/pgblitz/upload=NC:/mnt/tdrive=NC:/mnt/gdrive=NC:/mnt/tcrypt=NC:/mnt/gcrypt=NC /mnt/unionfs
+$dlpath/pgblitz/upload=NC:/mnt/tdrive=NC:/mnt/gdrive=NC:/mnt/tcrypt=NC:/mnt/gcrypt=NC:$dlpath/move=RW:$dlpath/downloads=NC /mnt/unionfs
