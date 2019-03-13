9a8239e3f0851892b9982751bf059fa9e70d4430
Admin9705 <user@dunn.cloud>
2019-03-12 22:47:27 -0400

    update

---
+++ functions/deploymove.sh
@@ -0,0 +1,167 @@
+#!/bin/bash
+#
+# Title:      PGBlitz (Reference Title File)
+# Author(s):  Admin9705
+# URL:        https://pgblitz.com - http://github.pgblitz.com
+# GNU:        General Public License v3.0
+################################################################################
+deploymove () {
+  deployblitzstartcheck
+# RCLONE BUILD
+echo "#------------------------------------------" > /opt/appdata/plexguide/rclone.conf
+echo "#RClone Rewrite | Visit https://pgblitz.com" >> /opt/appdata/plexguide/rclone.conf
+echo "#------------------------------------------" >> /opt/appdata/plexguide/rclone.conf
+
+cat /opt/appdata/plexguide/.gdrive >> /opt/appdata/plexguide/rclone.conf
+
+if [[ $(cat "/opt/appdata/plexguide/.gcrypt") != "NOT-SET" ]]; then
+echo ""
+cat /opt/appdata/plexguide/.gcrypt >> /opt/appdata/plexguide/rclone.conf; fi
+
+cat /opt/appdata/plexguide/.tdrive >> /opt/appdata/plexguide/rclone.conf
+
+if [[ $(cat "/opt/appdata/plexguide/.tcrypt") != "NOT-SET" ]]; then
+echo ""
+cat /opt/appdata/plexguide/.tcrypt >> /opt/appdata/plexguide/rclone.conf; fi
+
+cat /opt/appdata/plexguide/.keys >> /opt/appdata/plexguide/rclone.conf
+
+deployblitzcheck
+}
+
+deployblitzcheck () {
+
+tee <<-EOF
+
+━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━
+🚀 Conducting RClone Mount Checks ~ pgclone.pgblitz.com
+━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━
+EOF
+ginital=$(rclone lsd --config /opt/appdata/plexguide/rclone.conf gdrive: | grep -oP plexguide | head -n1)
+tinital=$(rclone lsd --config /opt/appdata/plexguide/rclone.conf tdrive: | grep -oP plexguide | head -n1)
+kinital=$(rclone lsd --config /opt/appdata/plexguide/rclone.conf GDSA01: | grep -oP plexguide | head -n1)
+
+if [[ "$ginital" != "plexguide" ]]; then
+  rclone mkdir --config /opt/appdata/plexguide/rclone.conf gdrive:/plexguide; fi
+if [[ "$tinital" != "plexguide" ]]; then
+  rclone mkdir --config /opt/appdata/plexguide/rclone.conf tdrive:/plexguide; fi
+if [[ "$kinital" != "plexguide" ]]; then
+  rclone mkdir --config /opt/appdata/plexguide/rclone.conf GDSA01:/plexguide; fi
+
+gsecond=$(rclone lsd --config /opt/appdata/plexguide/rclone.conf gdrive: | grep -oP plexguide | head -n1)
+tsecond=$(rclone lsd --config /opt/appdata/plexguide/rclone.conf tdrive: | grep -oP plexguide | head -n1)
+ksecond=$(rclone lsd --config /opt/appdata/plexguide/rclone.conf GDSA01: | grep -oP plexguide | head -n1)
+
+fail=0
+echo ""
+if [[ "$gsecond" == "plexguide" ]]; then echo "GDRIVE: Passed"; else echo "GDRIVE: Failed" && fail=1; fi
+if [[ "$tsecond" == "plexguide" ]]; then echo "TDRIVE: Passed"; else echo "GDRIVE: Failed" && fail=1; fi
+if [[ "$ksecond" == "plexguide" ]]; then echo "GDSA01: Passed"; else echo "GDSA01: Failed" && fail=1; fi
+
+if [[ "$fail" == "0" ]]; then
+  echo ""
+  read -p '↘️  BLITZ DEPLOY NOT READY ~ PASSED TEST | Press [ENTER] ' typed2 < /dev/tty
+  #pgblitzpass
+else
+tee <<-EOF
+
+━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━
+🚀 RClone Mount Checks - Failed
+━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━
+
+CANNOT DEPLOY PGBLITZ!
+
+POSSIBLE REASONS:
+1. GSuite Account is no longer valid or suspended
+2. User forgot to share out GDSA E-Mail to Team Drive
+3. Conducted a blitz key restore and keys are no longer valid
+
+━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━
+EOF
+read -p '↘️  Acknowledge Info | Press [ENTER] ' typed2 < /dev/tty
+clonestart
+fi
+}
+
+########################################################################################
+
+deploypgmove () {
+# RCLONE BUILD
+echo "#------------------------------------------" > /opt/appdata/plexguide/rclone.conf
+echo "#RClone Rewrite | Visit https://pgblitz.com" >> /opt/appdata/plexguide/rclone.conf
+echo "#------------------------------------------" >> /opt/appdata/plexguide/rclone.conf
+
+cat /opt/appdata/plexguide/.gdrive > /opt/appdata/plexguide/rclone.conf
+
+if [[ $(cat "/opt/appdata/plexguide/.gcrypt") != "NOT-SET" ]]; then
+echo ""
+cat /opt/appdata/plexguide/.gcrypt >> /opt/appdata/plexguide/rclone.conf; fi
+deploymovecheck
+}
+
+deploymovecheck () {
+
+tee <<-EOF
+
+━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━
+🚀 Conducting RClone Mount Checks ~ pgclone.pgblitz.com
+━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━
+
+EOF
+
+ginital=$(rclone lsd --config /opt/appdata/plexguide/rclone.conf gdrive: | grep -oP plexguide | head -n1)
+
+if [[ "$ginital" != "plexguide" ]]; then
+  rclone mkdir --config /opt/appdata/plexguide/rclone.conf gdrive:/plexguide; fi
+
+gsecond=$(rclone lsd --config /opt/appdata/plexguide/rclone.conf gdrive: | grep -oP plexguide | head -n1)
+
+if [[ "$gsecond" == "plexguide" ]]; then echo "GDRIVE: Passed"; else echo "GDRIVE: Failed"; fi
+
+if [[ "$gsecond" == "plexguide" ]]; then
+  echo ""
+  read -p '↘️  PG MOVE DEPLOYMENT NOT READY | Press [ENTER]' typed2 < /dev/tty
+  #pgblitzpass
+else
+tee <<-EOF
+
+━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━
+🚀 RClone Mount Checks - Failed
+━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━
+
+CANNOT DEPLOY PGMOVE!
+
+POSSIBLE REASONS:
+1. GSuite Account is no longer valid or suspended
+2. Keys are Invalid
+
+━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━
+EOF
+read -p '↘️  Acknowledge Info | Press [ENTER] ' typed2 < /dev/tty
+clonestart
+fi
+}
+
+deployblitzstartcheck () {
+
+pgclonevars
+if [[ "$displaykey" == "0" ]]; then
+tee <<-EOF
+
+━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━
+🌎 Fail Notice ~ pgclone.pgblitz.com
+━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━
+
+💬  There are [0] keys generated for PG Blitz! Create those first!
+
+NOTE: Without any keys, PG Blitz cannot upload any data without the use
+of service accounts
+
+━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━
+EOF
+
+read -p '↘️  Acknowledge Info | Press [ENTER] ' typed < /dev/tty
+clonestart
+fi
+
+}
