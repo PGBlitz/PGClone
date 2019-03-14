02547fb8fb8a3363244cd4da0fbb803c8af378aa
Admin9705 <user@dunn.cloud>
2019-03-14 09:32:06 -0400

    update

--- scripts/pgmove.sh
+++ scripts/pgmove.sh
@@ -5,11 +5,13 @@
 # URL:        https://pgblitz.com - http://github.pgblitz.com
 # GNU:        General Public License v3.0
 ################################################################################
+# NOTES
+# Variables come from what's being called from deploymove.sh under functions
+################################################################################
 if pidof -o %PPID -x "$0"; then
    exit 1
 fi
-# Outside Variables
-dlpath=$(cat /var/plexguide/server.hd.path)
+
 sleep 10
 while true
 do
@@ -18,7 +20,7 @@ dlpath=$(cat /var/plexguide/server.hd.path)
 
 ## BWLIMIT 9 and Lower Prevents Google 750GB Google Upload Ban
 
-rclone moveto "$dlpath/downloads/" "$dlpath/move/" \
+rclone moveto "$hdpath/downloads/" "$hdpath/move/" \
 --config /opt/appdata/plexguide/rclone.conf \
 --log-file=/var/plexguide/logs/pgmove.log \
 --log-level ERROR --stats 5s --stats-file-name-length 0 \
@@ -32,7 +34,7 @@ rclone moveto "$dlpath/downloads/" "$dlpath/move/" \
 --exclude="**handbrake**" --exclude="**bazarr**" \
 --exclude="**ignore**"  --exclude="**inProgress**"
 
-rclone move "$dlpath/move/" "{{type}}:/" \
+rclone move "$hdpath/move/" "{{type}}:/" \
 --config /opt/appdata/plexguide/rclone.conf \
 --log-file=/var/plexguide/logs/pgmove.log \
 --log-level INFO --stats 5s --stats-file-name-length 0 \
@@ -46,8 +48,8 @@ rclone move "$dlpath/move/" "{{type}}:/" \
 sleep 5
 
 # Remove empty directories
-find "$dlpath/downloads" -mindepth 2 -mmin +5 -type d -empty -exec rm -rf {} \;
-find "$dlpath/downloads" -mindepth 3 -mmin +360 -type d -size -100M -exec rm -rf {} \;
-find "$dlpath/move" -mindepth 2 -mmin +5 -type d -empty -delete
+find "$hdpath/downloads" -mindepth 2 -mmin +5 -type d -empty -exec rm -rf {} \;
+find "$hdpath/downloads" -mindepth 3 -mmin +360 -type d -size -100M -exec rm -rf {} \;
+find "$hdpath/move" -mindepth 2 -mmin +5 -type d -empty -delete
 
 done
