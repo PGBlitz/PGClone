a158681b2d2482af58ef1caf085b9aaff363b792
Admin9705 <user@dunn.cloud>
2019-03-14 23:12:57 -0400

    update

--- functions/deploy.sh
+++ functions/deploy.sh
@@ -140,20 +140,20 @@ gsecond=$(rclone lsd --config /opt/appdata/plexguide/rclone.conf gdrive: | grep 
 # For Encryption
 if [[ "transport" == "me" ]]; then
 c1inital=$(rclone lsd --config /opt/appdata/plexguide/rclone.conf gdrive: | grep -oP encrypt | head -n1)
-c2inital=$(rclone lsd --config /opt/appdata/plexguide/rclone.conf gcrypt: | grep -oP encrypt | head -n1); fi
+c2inital=$(rclone lsd --config /opt/appdata/plexguide/rclone.conf gcrypt: | grep -oP plexguide | head -n1); fi
 
 # For Encryption
-if [[ "$c1inital" != "encrypt" && "transport" == "me" ]]; then
+if [[ "$c1inital" != "encrypt" && "$transport" == "me" ]]; then
   rclone mkdir --config /opt/appdata/plexguide/rclone.conf gdrive:/encrypt; fi
-if [[ "$c2inital" != "plexguide" && "transport" == "me" ]]; then
+if [[ "$c2inital" != "plexguide" && "$transport" == "me" ]]; then
   rclone mkdir --config /opt/appdata/plexguide/rclone.conf gcrypt:/plexguide; fi
 
 if [[ "$gsecond" == "plexguide" ]]; then echo "GDRIVE: Passed"; else echo "GDRIVE: Failed" && fail=1; fi
 
 # For Encryption
 if [[ "$transport" == "me" ]]; then
-if [[ "$c1inital" == "encrypt" ]]; then echo "CRYPT2: Passed"; else echo "CRYPT2: Failed" && fail=1; fi
-if [[ "$c2inital" == "plexguide" ]]; then echo "CRYPT3: Passed"; else echo "CRYPT3: Failed" && fail=1; fi; fi
+if [[ "$c1inital" == "encrypt" ]]; then echo "CRYPT1: Passed"; else echo "CRYPT1: Failed" && fail=1; fi
+if [[ "$c2inital" == "plexguide" ]]; then echo "CRYPT2: Passed"; else echo "CRYPT2: Failed" && fail=1; fi; fi
 
 if [[ "$gfail" != "1" ]]; then
 
