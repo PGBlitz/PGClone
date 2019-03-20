e2ae2eabd3654c60832fe22f6a6ea859d803ff85
Admin9705 <user@dunn.cloud>
2019-03-20 11:08:57 -0400

    update

--- functions/mountnumbers.sh
+++ functions/mountnumbers.sh
@@ -144,6 +144,15 @@ if [[ "$typed" -lt "$start1" || "$typed" -gt "$end1" ]]; then mountset; else
   if [[ "$mountselection" == "5" ]]; then echo "$typed" > /var/plexguide/vfs_rcs; fi
   if [[ "$mountselection" == "6" ]]; then echo "$typed" > /var/plexguide/vfs_rcsl; fi
 
+tee <<-EOF
+
+━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━
+Information Saved!
+━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━
+
+EOF
+read -rp '↘️  Input Selection | Press [ENTER]: ' typed < /dev/tty
+
 fi
 
 mountnumbers
