fa2904734b4d931ef1f4a6a277edf8d8b41f0748
Admin9705 <user@dunn.cloud>
2019-03-27 00:49:04 -0400

    update

---
+++ functions/deploylocal.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+#
+# Title:      PGBlitz (Reference Title File)
+# Author(s):  Admin9705
+# URL:        https://pgblitz.com - http://github.pgblitz.com
+# GNU:        General Public License v3.0
+################################################################################
+# NOTES
+# Variable recall comes from /functions/variables.sh
+################################################################################
+executelocal () {
+
+# Reset Front Display
+rm -rf plexguide/deployed.version
+
+# Call Variables
+pgclonevars
+
+# to remove all service running prior to ensure a clean launch
+ansible-playbook /opt/pgclone/ymls/remove.yml
+
+# deploy union
+ansible-playbook /opt/pgclone/ymls/pgunion.yml -e "\
+  transport=$transport \
+  multihds=$multihds
+  type=$type
+  hdpath=$hdpath"
+
+tee <<-EOF
+
+━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━
+💪 DEPLOYED: PG Local Edition
+━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━
+EOF
+
+read -rp '↘️  Acknowledge Info | Press [ENTER] ' typed < /dev/tty
+
+}
