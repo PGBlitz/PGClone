ffeb8baaec0697fd7d66831a1766f765168142fd
Robert Baker <se7enalive@gmail.com>
2019-04-17 02:00:40 -0700

    fix

--- scripts/pgmove.sh
+++ scripts/pgmove.sh
@@ -54,7 +54,7 @@ rclone move "{{hdpath}}/move/" "{{type}}:/" \
 --tpslimit 6 \
 --checkers=16 \
 --max-size=300G \
---drive-chunk-size={{vfs_dcs}} \
+--drive-chunk-size={{vfs_dcs}}M \
 --exclude="**_HIDDEN~" --exclude=".unionfs/**" \
 --exclude='**partial~' --exclude=".unionfs-fuse/**" \
 --exclude=".fuse_hidden**" \
