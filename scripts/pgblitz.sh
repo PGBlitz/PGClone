d46331eaf94de333e55640ec2248a59d2fb655e1
MrDoobPG <46342172+MrDoobPG@users.noreply.github.com>
2019-05-17 16:09:05 +0200

    exclude list added for torrent user
     $(printf "! -name %s " $(cat /opt/pgclone/functions/exclude))

--- scripts/pgblitz.sh
+++ scripts/pgblitz.sh
@@ -76,8 +76,8 @@ while read p; do
   find "{{hdpath}}/move/" -mindepth 2 -type d -mmin +2 -empty -exec rm -rf {} \;
 
   # Removes garbage
-  find "{{hdpath}}/downloads" -mindepth 2 -type d -cmin +$cleaner -empty -exec rm -rf {} \;
-  find "{{hdpath}}/downloads" -mindepth 2 -type f -cmin +$cleaner -size +1M -exec rm -rf {} \;
+  find "{{hdpath}}/downloads" -mindepth 2 -type d -cmin +$cleaner  $(printf "! -name %s " $(cat /opt/pgclone/functions/exclude)) -empty -exec rm -rf {} \;
+  find "{{hdpath}}/downloads" -mindepth 2 -type f -cmin +$cleaner  $(printf "! -name %s " $(cat /opt/pgclone/functions/exclude)) -size +1M -exec rm -rf {} \;
 
 done </var/plexguide/.blitzfinal
 }
