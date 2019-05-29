b143af3aaa04da88e947a6139067780219b5f121
MrDoobPG <46342172+MrDoobPG@users.noreply.github.com>
2019-05-29 17:16:21 +0200

    Added fix for xTeve records
    --exclude="**.grab/**"

--- scripts/pgblitz.sh
+++ scripts/pgblitz.sh
@@ -39,7 +39,7 @@ while read p; do
   --log-level ERROR --stats 5s --stats-file-name-length 0 \
   --exclude="**_HIDDEN~" --exclude=".unionfs/**" \
   --exclude='**partial~' --exclude=".unionfs-fuse/**" \
-  --exclude=".fuse_hidden**" \
+  --exclude=".fuse_hidden**" --exclude="**.grab/**" \
   --exclude="**sabnzbd**" --exclude="**nzbget**" \
   --exclude="**qbittorrent**" --exclude="**rutorrent**" \
   --exclude="**deluge**" --exclude="**transmission**" \
@@ -63,7 +63,7 @@ while read p; do
   --drive-chunk-size={{vfs_dcs}}M \
   --exclude="**_HIDDEN~" --exclude=".unionfs/**" \
   --exclude='**partial~' --exclude=".unionfs-fuse/**" \
-  --exclude=".fuse_hidden**" \
+  --exclude=".fuse_hidden**" --exclude="**.grab/**"  \
   --exclude="**sabnzbd**" --exclude="**nzbget**" \
   --exclude="**qbittorrent**" --exclude="**rutorrent**" \
   --exclude="**deluge**" --exclude="**transmission**" \
