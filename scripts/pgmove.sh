6ee5331376efe6a9c05458df943ac4353784f4a9
MrDoobPG <46342172+MrDoobPG@users.noreply.github.com>
2019-06-02 14:11:44 +0200

    Exclude  fix from ' to "
    Push update

--- scripts/pgmove.sh
+++ scripts/pgmove.sh
@@ -40,7 +40,7 @@ rclone moveto "{{hdpath}}/downloads/" "{{hdpath}}/move/" \
 --log-file=/var/plexguide/logs/pgmove.log \
 --log-level ERROR --stats 5s --stats-file-name-length 0 \
 --exclude="**_HIDDEN~" --exclude=".unionfs/**" \
---exclude='**partial~' --exclude=".unionfs-fuse/**" \
+--exclude="**partial~" --exclude=".unionfs-fuse/**" \
 --exclude=".fuse_hidden**" --exclude="**.grab/**" \
 --exclude="**sabnzbd**" --exclude="**nzbget**" \
 --exclude="**qbittorrent**" --exclude="**rutorrent**" \
@@ -63,7 +63,7 @@ rclone move "{{hdpath}}/move/" "{{type}}:/" \
 --drive-chunk-size={{vfs_dcs}}M \
 --user-agent="$useragent" \
 --exclude="**_HIDDEN~" --exclude=".unionfs/**" \
---exclude='**partial~' --exclude=".unionfs-fuse/**" \
+--exclude="**partial~" --exclude=".unionfs-fuse/**" \
 --exclude=".fuse_hidden**" --exclude="**.grab/**" \
 --exclude="**sabnzbd**" --exclude="**nzbget**" \
 --exclude="**qbittorrent**" --exclude="**rutorrent**" \
