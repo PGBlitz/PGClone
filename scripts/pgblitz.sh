0a863cde4e89b1b911209a681cd6c82844838464
MrDoobPG <46342172+MrDoobPG@users.noreply.github.com>
2019-06-20 07:12:38 +0200

    Update pgblitz.sh
    Again

--- scripts/pgblitz.sh
+++ scripts/pgblitz.sh
@@ -71,7 +71,7 @@ startscript () {
         sleep 30
 
         # Remove empty directories
-        find "{{hdpath}}/move" -type d -mmin +2 -empty -exec rmdir \{} \;
+        find "{{hdpath}}/move" -mindepth 1 -type d -mmin +2 -empty -exec rmdir \{} \;
         find "{{hdpath}}downloads" -mindepth 2 -type d -cmin +$cleaner -empty -exec rmdir \{} \;
         
         # nzb cleanup, delete files < 3G
