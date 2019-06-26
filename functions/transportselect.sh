92891279a5043c8b843b004488804e007e734803
LooseSeal2 <lucile2@protonmail.com>
2019-06-25 19:14:44 -0700

    update transportselect

--- functions/transportselect.sh
+++ functions/transportselect.sh
@@ -27,19 +27,19 @@ read -rp '↘️  Input Selection | Press [ENTER]: ' typed < /dev/tty
 case $typed in
     1 )
     echo "mu" > /var/plexguide/pgclone.transport
-    echo "PGMove Unencrypt" > /var/plexguide/pg.transport ;;
+    echo "Move" > /var/plexguide/pg.transport ;;
     2 )
     echo "me" > /var/plexguide/pgclone.transport
-    echo "PGMove Encrypted" > /var/plexguide/pg.transport ;;   
+    echo "Move Encrypted" > /var/plexguide/pg.transport ;;   
     3 )
     echo "bu" > /var/plexguide/pgclone.transport
-    echo "PGBlitz Unencrypt" > /var/plexguide/pg.transport ;;
+    echo "Blitz" > /var/plexguide/pg.transport ;;
     4 )
     echo "be" > /var/plexguide/pgclone.transport
-    echo "PGBlitz Encrypted" > /var/plexguide/pg.transport ;;    
+    echo "Blitz Encrypted" > /var/plexguide/pg.transport ;;    
     5 )
     echo "le" > /var/plexguide/pgclone.transport
-    echo "PG Local Edition" > /var/plexguide/pg.transport ;;    
+    echo "Local Edition" > /var/plexguide/pg.transport ;;    
     * )
         transportselect ;;
 esac
