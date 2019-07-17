100f53ea827f3f5fc63871d2a7ead75471a88b7d
LooseSeal2 <lucile2@protonmail.com>
2019-07-17 12:28:09 -0700

    fix

--- functions/deploy.sh
+++ functions/deploy.sh
@@ -227,16 +227,16 @@ cleanmounts() {
 
   echo "checking for empty mounts..."
   if [ -d "/mnt/unionfs" ]; then
-    pgunion_size=$(du -s /mnt/unionfs | cut -f1 | bc -l | rev | cut -c 2- | rev)
-    if [ $pgunion_size -gt 4 ]; then
+    pgunion_check=$(ls -a /mnt/unionfs | wc -l)
+    if [[ $pgunion_check != 2 ]]; then
       echo "pgunion is not empty when unmounted, fixing..."
       mv -f /mnt/unionfs/* /mnt/move/
       rm -rf /mnt/unionfs/*
     fi
   fi
   if [ -d "/mnt/gdrive" ]; then
-    gdrive_size=$(du -s /mnt/gdrive | cut -f1 | bc -l | rev | cut -c 2- | rev)
-    if [ $gdrive_size -gt 4 ]; then
+    gdrive_check=$(ls -a /mnt/gdrive | wc -l)
+    if [[ $gdrive_check != 2 ]]; then
       echo "gdrive is not empty when unmounted, fixing..."
       mv -f /mnt/gdrive/* /mnt/move/
       rm -rf /mnt/gdrive/*
@@ -244,24 +244,24 @@ cleanmounts() {
   fi
 
   if [ -d "/mnt/gcrypt" ]; then
-    gcrypt_size=$(du -s /mnt/gcrypt | cut -f1 | bc -l | rev | cut -c 2- | rev)
-    if [ $gcrypt_size -gt 4 ]; then
+    gcrypt_check=$(ls -a /mnt/gcrypt | wc -l)
+    if [[ $gcrypt_check != 2 ]]; then
       echo "gcrypt is not empty when unmounted, fixing..."
       mv -f /mnt/gcrypt/* /mnt/move/
       rm -rf /mnt/gcrypt/*
     fi
   fi
   if [ -d "/mnt/tdrive" ]; then
-    tdrive_size=$(du -s /mnt/tdrive | cut -f1 | bc -l | rev | cut -c 2- | rev)
-    if [ $tdrive_size -gt 4 ]; then
+    tdrive_check=$(ls -a /mnt/tdrive | wc -l)
+    if [[ $tdrive_check != 2 ]]; then
       echo "tdrive is not empty when unmounted, fixing..."
       mv -f /mnt/tdrive/* /mnt/move/
       rm -rf /mnt/tdrive/*
     fi
   fi
   if [ -d "/mnt/tcrypt" ]; then
-    tcrypt_size=$(du -s /mnt/tcrypt | cut -f1 | bc -l | rev | cut -c 2- | rev)
-    if [ $tcrypt_size -gt 4 ]; then
+    tcrypt_check=$(ls -a /mnt/tcrypt | wc -l)
+    if [[ $tcrypt_check != 2 ]]; then
       echo "tcrypt is not empty when unmounted, fixing..."
       mv -f /mnt/tcrypt/* /mnt/move/
       rm -rf /mnt/tcrypt/*
