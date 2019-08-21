7d30f37f5431fbbbb1dda3d342512ebcc8f2d6a3
Admin9705 <24727006+Admin9705@users.noreply.github.com>
2019-08-21 07:46:43 -0400

    update

--- scripts/pgmove.sh
+++ scripts/pgmove.sh
@@ -13,78 +13,39 @@ if pidof -o %PPID -x "$0"; then
    exit 1
 fi
 
+useragent="$(cat /pg/var/uagent)"
+cleaner="$(cat /pg/var/cloneclean)"
 touch /pg/logs/pgmove.log
-
-echo "" >> /pg/logs/pgmove.log
-echo "" >> /pg/logs/pgmove.log
-echo "----------------------------" >> /pg/logs/pgmove.log
-echo "PG Move Log - First Startup" >> /pg/logs/pgmove.log
 
 chown -R 1000:1000 "{{hdpath}}/transfer"
 chmod -R 775 "{{hdpath}}/transfer"
 
-sleep 10
-while true
-do
+find /pg/transfer/ -type f > /pg/logs/.temp_list
 
-# Repull excluded folder
-# wget -qN https://raw.githubusercontent.com/PGBlitz/PGClone/v8.6/functions/exclude -P /pg/var/
+while read p; do
+  sed -i "/^$p\b/Id" /pg/logs/.temp_list
+done </pg/logs/.transfer_list
 
-find /pg/transfer/ -type f
+head -n +1 /pg/logs/.temp_list >> /pg/logs/.transfer_list
+uploadfile=$(cat "head -n +1 /pg/logs/.temp_list")
 
+chown 1000:1000 "$uploadfile"
+chmod 775 "$uploadfile"
 
-  cleaner="$(cat /pg/var/cloneclean)"
-  useragent="$(cat /pg/var/uagent)"
-
-rclone moveto "{{hdpath}}/downloads/" "{{hdpath}}/transfer/" \
+rclone move "$uploadfile" "{{type}}:/" \
 --config /pg/rclone/blitz.conf \
---log-file=/pg/logs/pgmove.log \
---log-level ERROR --stats 5s --stats-file-name-length 0 \
---exclude="**_HIDDEN~" --exclude=".unionfs/**" \
---exclude="**partial~" --exclude=".unionfs-fuse/**" \
---exclude=".fuse_hidden**" --exclude="**.grab/**" \
---exclude="**sabnzbd**" --exclude="**nzbget**" \
---exclude="**qbittorrent**" --exclude="**rutorrent**" \
---exclude="**deluge**" --exclude="**transmission**" \
---exclude="**jdownloader**" --exclude="**makemkv**" \
---exclude="**handbrake**" --exclude="**bazarr**" \
---exclude="**ignore**"  --exclude="**inProgress**"
-
-chown -R 1000:1000 "{{hdpath}}/move"
-chmod -R 775 "{{hdpath}}/move"
-
-rclone move "{{hdpath}}/transfer/" "{{type}}:/" \
---config /pg/rclone/blitz.conf \
---log-file=/pg/logs/pgmove.log \
+--log-file=/pg/logs/pgtransfer.log \
 --log-level INFO --stats 5s --stats-file-name-length 0 \
 --bwlimit {{bandwidth.stdout}}M \
 --tpslimit 6 \
 --checkers=16 \
 --drive-chunk-size={{dcs}} \
 --user-agent="$useragent" \
---exclude="**_HIDDEN~" --exclude=".unionfs/**" \
---exclude="**partial~" --exclude=".unionfs-fuse/**" \
---exclude=".fuse_hidden**" --exclude="**.grab/**" \
---exclude="**sabnzbd**" --exclude="**nzbget**" \
---exclude="**qbittorrent**" --exclude="**rutorrent**" \
---exclude="**deluge**" --exclude="**transmission**" \
---exclude="**jdownloader**" --exclude="**makemkv**" \
---exclude="**handbrake**" --exclude="**bazarr**" \
---exclude="**ignore**"  --exclude="**inProgress**"
+--exclude="**_HIDDEN~" --exclude="**partial~"  \
+--exclude=".fuse_hidden**" --exclude="**.grab/**"
 
- sleep 30
+find "{{hdpath}}/transfer/" -mindepth 2 -type d -mmin +2 -empty -exec rm -rf {} \;
 
-  #Quick fix
-  # Remove empty directories
-  #find "$dlpath/downloads/" -mindepth 2 -type d -empty -exec rm -rf {} \;
-  #find "$dlpath/move/" -type d -empty -exec rm -rf {} \;
-  #find "$dlpath/move/" -mindepth 2 -type f -cmin +5 -size +1M -exec rm -rf {} \;
-
-  # Remove empty directories
-  find "{{hdpath}}/transfer/" -mindepth 2 -type d -mmin +2 -empty -exec rm -rf {} \;
-
-  # Removes garbage | torrent folder excluded
-  find "{{hdpath}}/downloads" -mindepth 2 -type d -cmin +$cleaner  $(printf "! -name %s " $(cat /pg/var/exclude)) -empty -exec rm -rf {} \;
-  find "{{hdpath}}/downloads" -mindepth 2 -type f -cmin +$cleaner  $(printf "! -name %s " $(cat /pg/var/exclude)) -size +1M -exec rm -rf {} \;
-
-done
+# Removes garbage | torrent folder excluded
+find "{{hdpath}}/downloads" -mindepth 2 -type d -cmin +$cleaner  $(printf "! -name %s " $(cat /pg/var/exclude)) -empty -exec rm -rf {} \;
+find "{{hdpath}}/downloads" -mindepth 2 -type f -cmin +$cleaner  $(printf "! -name %s " $(cat /pg/var/exclude)) -size +1M -exec rm -rf {} \;
