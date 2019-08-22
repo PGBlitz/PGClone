d256e4b1e2900f996bc9ee2056c2d9234bf08059
Admin9705 <24727006+Admin9705@users.noreply.github.com>
2019-08-22 10:02:16 -0400

    update

--- scripts/pgmove.sh
+++ scripts/pgmove.sh
@@ -22,6 +22,7 @@ touch /pg/logs/.temp_list
 
 filecount=$(wc -l /pg/logs/.transfer_list | awk '{print $1}')
 echo "$filecount" > /pg/var/filecount
+if [[ "$filecount" -gt 8 ]]; then exit; fi
 
 find /pg/transfer/ -type f > /pg/logs/.temp_list
 
@@ -32,7 +33,7 @@ done </pg/logs/.transfer_list
 head -n +1 /pg/logs/.temp_list >> /pg/logs/.transfer_list
 uploadfile=$(head -n +1 /pg/logs/.temp_list)
 
-#if [[ "$filecount" -gt 8 || "$filecount" -lt "1" ]]; then exit; fi
+
 if [[ "$uploadfile" == "" ]]; then exit; fi
 
 chown 1000:1000 "$uploadfile"
