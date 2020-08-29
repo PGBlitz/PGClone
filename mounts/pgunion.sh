5adc914f1338001b02452c5e4ffc83295977958b
doob187 <60312740+doob187@users.noreply.github.com>
2020-08-29 12:56:20 +0200

    Update pgunion.sh

--- mounts/pgunion.sh
+++ mounts/pgunion.sh
@@ -9,4 +9,4 @@ sleep 2
 
 hdpath="$(cat /var/plexguide/server.hd.path)"
 mergerfs -o sync_read,auto_cache,dropcacheonclose=true,use_ino,allow_other,func.getattr=newest,category.create=ff,minfreespace=0,fsname=pgunion \
-$hdpath/downloads=RW:{{multihds}}/mnt/tdrive=NC:/mnt/gdrive=NC:/mnt/tcrypt=NC:/mnt/gcrypt=NC /mnt/unionfs
+{{multihds}}/mnt/tdrive=NC:/mnt/gdrive=NC:/mnt/tcrypt=NC:/mnt/gcrypt=NC /mnt/unionfs
